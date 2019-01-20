3d3ee919d091970675ce64832f4af6d9a9f2504e
Mark Condello <markcondello@Marks-MacBook-Pro.local>
2019-01-20 12:26:35 +1100

    Analytics tracking plugin.

--- ch2-plugin-header/ch2-plugin-header.php
+++ ch2-plugin-header/ch2-plugin-header.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name:	Chapter 2 - Plugin Header
+Plugin Name:	Chapter 2 - Plugin Header Output
 Plugin URI:
 Description:	Declares a plugin that will be visible in the WordPress admin interface
 Version:		1.0
@@ -8,3 +8,18 @@ Author:			Yannick Lefebvre
 Author URI:		http://ylefebvre.ca
 License:		GPLv2
 */
+function ch2pho_page_header_output(){
+?>
+<!-- Global Site Tag (gtag.js) - Google Analytics | Using markcondello.com.au account tracking id -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-126447053-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-126447053-1');
+</script>
+<?php
+    
+}
+
+add_action('wp_head', 'ch2pho_page_header_output');
