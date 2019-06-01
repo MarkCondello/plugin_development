adc6049a24fd4a87ed61f98d3859eacfce0501a3
Mark Condello <markcondello@Marks-MacBook-Pro.local>
2019-06-01 22:27:01 +1000

    Completed social media plugin.

--- AGSocialMedia/agsocialmedia.php
+++ AGSocialMedia/agsocialmedia.php
@@ -23,7 +23,7 @@ function agsml_settings_link($links, $file){
     if(!$this_plugin) $this_plugin = plugin_basename(__FILE__);
 
     if($file == $this_plugin) {
-        $settings_link = '<a href="options-general.php?page=AGSsocialMedia/antelope-social-media-link.php">' .__("Settings", "agsml_social_media") . '</a>';
+        $settings_link = '<a href="options-general.php?page=AGSsocialMedia/antelope-social-media-link.php">' . __("Settings", "agsml_social_media") . '</a>';
         array_unshift($links, $settings_link);
     }
     return $links;
@@ -40,7 +40,7 @@ function agsml_register_settings(){
 }
 
 function agsml_settings_page() { 
-    /* the inputs use form_option() which sanitizes data received */
+/* the inputs use form_option() which sanitizes data received */
 ?>
     <div class="wrap agsml_social_list">
         <h2>Antelope General Social Media Links</h2>
@@ -87,8 +87,84 @@ function agsml_settings_page() {
 }
 
 function agsml_enqueue_styles(){
-     wp_enqueue_style('agsml_stylesheet', plugins_url('/agsml-stylesheet.css', __FILE__ ));  
+    $plugin_url = plugin_dir_url( __FILE__ );
+    wp_enqueue_style('agsml_stylesheet', $plugin_url . '/agsml-stylesheet.css' );  
 }
 add_action('wp_enqueue_scripts', 'agsml_enqueue_styles' );
+
+
+/* Register the widget */
+function agsml_register_widget() {
+    register_widget( 'Antelope_Widget' );
+}
+
+
+
+class Antelope_Widget extends WP_Widget {
+    public function __construct() {
+        //widget code than contains function logic
+/*         $widget_ops = array(
+            'classname' => 'agsml_widget',
+            'description' => __('Your social media links', 'agsml')
+        );
+        parent::__construct(false, $name = "AG Social Media Links", $widget_ops); */
+        parent::__construct( 'Antelope_Widget', __('Antelope Widget', 'agsml'), array(
+            'classname'   => 'agsml_widget',
+            'description' => __('Your social media links', 'agsml'),
+            )
+          );
+    }
+
+    public function widget($args, $instance){
+        //display the widget on website
+        extract($args);
+        $title = apply_filters('widget_title', $instance['title']);
+        echo $before_widget;
+        if($title){
+            echo $before_title . $title . $after_title;
+        }
+        $social_list = '<ul>';
+        if(get_option('agsml_facebook')){
+            $social_list .= '<li class="facebook"><a href="' . get_option('agsml_facebook') . '"> '. __('Friend us on FB', 'agsml') .'</a></li>';
+        }
+        if(get_option('agsml_twitter')){
+            $social_list .= '<li class="twitter"><a href="' . get_option('agsml_twitter') . '"> '. __('Follow us on Twitter', 'agsml') .'</a></li>';
+        }
+        if(get_option('agsml_linkedin')){
+            $social_list .= '<li class="linkedin"><a href="' . get_option('agsml_linkedin') . '"> '. __('Connect on Linkedin', 'agsml') .'</a></li>';
+        }
+        if(get_option('agsml_youtube')){
+            $social_list .= '<li class="youtube"><a href="' . get_option('agsml_youtube') . '"> '. __('Watch us on Youtube', 'agsml') .'</a></li>';
+        }
+        $social_list .= '</ul>';
+        echo $social_list;
+        echo $after_widget;
+    }
+
+    public function update($new_instance, $old_instance){
+        //save widget options once settings change
+        $instance = $old_instance;
+        $instance['title'] = strip_tags($new_instance['title']);
+        return $instance;
+    }
+
+    public function form($instance){
+        //form to display the widget settings
+        $title = esc_attr($instance['title']);
+        ?>
+        <p> 
+            <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:');?></label>
+            <input class="widefat" id="<?php $this->get_field_id('title');?>"
+                name="<?php echo $this->get_field_name('title'); ?>"
+                type="text"
+                value="<?php echo $title; ?>" />
+        </p>
+        <?php
+    }
+
+}
+
+/* Load the widget */
+add_action('widgets_init', 'agsml_register_widget');
 
  ?>
