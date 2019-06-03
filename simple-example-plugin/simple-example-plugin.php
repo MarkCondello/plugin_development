643eb169a40b94a9ea11c611d7ba1885dadf1cfd
Mark Condello <markcondello@Marks-MacBook-Pro.local>
2019-06-03 22:10:20 +1000

    activate, deactivate and uninsall hooks

--- simple-example-plugin/simple-example-plugin.php
+++ simple-example-plugin/simple-example-plugin.php
@@ -25,15 +25,41 @@ with this program. If not, visit: https://www.gnu.org/licenses/
 function myplugin_action_hook_example(){
     wp_mail('email@example.com', 'Subject', 'Message...');
 }
-
 add_action('init', 'myplugin_action_hook_example');
 
 //init hook will run every time WP executes
 //life cycle of these hooks can be seen in the codex here: https://codex.wordpress.org/Plugin_API/Action_Reference
-
 function myplugin_filter_hook_example($content){
     $content = $content . "<p>My custom content...</p>";
     return $content;
 }
-
 add_filter('the_content', 'myplugin_filter_hook_example');
+
+//do stuff on activation
+function myplugins_on_activation(){
+    //if user has privledges
+    if(! current_user_can('activate_plugins')) return;
+
+    add_option('myplugin_posts_per_page', 10);
+    add_option('myplugin_show_welcome_page', true);
+}
+register_deactivation_hook(__FILE__, 'myplugins_on_activation');
+
+//do stuff on deactivation
+function myplugins_on_deactivation(){
+    //this die statement should be removed after testings
+    wp_die("The plugin is deactivate");
+    //if user has privledges
+    if(! current_user_can('activate_plugins')) return;
+    flush_rewrite_rules();
+}
+register_activation_hook(__FILE__, 'myplugins_on_deactivation');
+
+//do stuff on uninstall
+function myplugins_on_uninstall(){
+      //if user has privledges
+    if(! current_user_can('activate_plugins')) return;
+    delete_option('myplugin_posts_per_page', 10);
+    delete_option('myplugin_show_welcome_page', true);
+}
+register_uninstall_hook(__FILE__, 'myplugins_on_uninstall');
